603e321ebfc2f0168134558a4b56a1ed70e1f924
Rmadyun <rmadyun@gmail.com>
2016-01-24 22:58:48 -0600

    Updated UI code
    Full Android Studio project source files are under
    UI_Code/TrainTrax\app\src\main\java\edu\uah\cpe.....

--- UI_Code/TrainTrax/app/src/main/java/TrackDiagram.java
+++ UI_Code/TrainTrax/app/src/main/java/TrackDiagram.java
@@ -1,12 +1,20 @@
 package edu.uah.cpe.traintrax;
-
+import java.util.ArrayList;
 
 public class TrackDiagram {
 
     /* private variable for number of people to split the bill with */
     private int numberOfCoordinates;
+    //drawRect(float left, float top, float right, float bottom, Paint paint)
+    private ArrayList recLeftCoordinate = new ArrayList();
+    private ArrayList recTopCoordinate = new ArrayList ();
+    private ArrayList rectRightCoordinate = new ArrayList ();
+    private ArrayList rectBottomCoordinate = new ArrayList();
 
-    /* function gets all the value of the coordinates for the track diagram */
+
+    /* function gets all the value of the coordinates for the track diagram
+       and stores them in the ArrayLists.  The view code will use the array
+        lists to draw rectangles on the view */
     public void getCoordinates(){
         return;
     }
@@ -16,6 +24,7 @@ public class TrackDiagram {
     /**
      * Constructor
      * @param
+
 
     /* Default Constructor */
     public TrackDiagram() {
--- UI_Code/TrainTrax/app/src/main/java/edu/uah/cpe/traintrax/MainActivity.java
+++ UI_Code/TrainTrax/app/src/main/java/edu/uah/cpe/traintrax/MainActivity.java
@@ -1,15 +1,20 @@
 package edu.uah.cpe.traintrax;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.app.Activity;
+import android.content.Context;
+
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     edu.uah.cpe.traintrax.TrackDiagram trackdata = new edu.uah.cpe.traintrax.TrackDiagram();
 
     @Override
@@ -50,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
         //you can override the other setTitle as well if you need in
         setTitle("Train Trax System");
     }
+
 
 
     }
