8b5aeaac1f4c10a9645fa68c8416918c4ab2a2cb
death <cathlorn@gmail.com>
2016-04-25 06:51:35 -0500

    Revert "Merge branch 'master' of https://github.com/Rmadyun/CPE656TL"
    This reverts commit d0dd282d3137efb5984d9e0ba3e633188f22da8b, reversing
    changes made to f61c0eac47a109b26728d5c2d0789764db58c046.

--- source/MDUsource/MDU_2.0/config.h
+++ source/MDUsource/MDU_2.0/config.h
@@ -5,7 +5,7 @@
 byte TRAIN_ID = 0x1a;  //This is ID used in messages. Should match the ID of the train in the Loconet commands for clarity. 2 is reserved.
 byte BASE_ID = 0x63;    //The ID for the base computer. Should verify that setting this to 0 is not an issue.
 
-enum mID: byte { IMU=3, RFID=4, RTT_REQ=5, RTT_RESP=6, ID = 7, TIME=8, TIME_RESP = 9, ID_RESP = 10 };
+enum mID: byte { IMU=3, RFID=4, RTT_REQ=5, RTT_RESP=6, ID = 7, TIME=8, TIME_RESP = 9 };
 
 const int MPU_addr=0x68;  // I2C address of the MPU-6050
 
--- source/MDUsource/MDU_2.0/measurementRelay.h
+++ source/MDUsource/MDU_2.0/measurementRelay.h
@@ -17,26 +17,69 @@ class measurementRelay
     void forwardIMU();
     void listenForMeasurements();
     void linkIMU(imuDriver * imu);
-  private:
-    void baseLink();
-    unsigned long measureRTT();
-    void timeLink(unsigned long RTT);
 };
-
-//**************************************************************************
-//
-//  Initialize by linking to base, determining RTT, and getting time
-//
-//**************************************************************************
 
 void measurementRelay::init()
 {
 
-  baseLink();                         //Used to announce MDU to base computer
+  Serial.println("test");
 
-  unsigned long cRTT = measureRTT();  //Callculate RTT to base computer
+  
+  byte IDmsg [4] = {BASE_ID, TRAIN_ID, ID, '\n'};         //Build ID message
+  Serial.write(IDmsg, 4);                                 //Send ID message
 
-  timeLink(cRTT);                     //Query time of day from base computer
+  unsigned long startTime, endTime;
+  unsigned long latency = 0;
+  byte RTTmsg[4] = {BASE_ID, TRAIN_ID, RTT_REQ, '\n'};    //Build RTT_REQ
+  bool rtttest = false;
+  for( int i = 0; i < 3; i++)                             //Send an RTT_REQ message 3 times
+  {
+    Serial.write(RTTmsg, 4);
+    startTime = millis();
+    while(rtttest == false)
+    {
+      while(!Serial){}
+      if(Serial.read() == TRAIN_ID)
+      {
+        Serial.read();
+        if(Serial.read() == RTT_RESP)                     //Recieve RTT_RESP
+        {
+          latency += (millis() - startTime);
+          rtttest = true;
+        }
+      }
+      
+    }
+    rtttest = false;
+  }
+
+  latency = latency/3;
+
+  byte TMEmsg [4] = {BASE_ID, TRAIN_ID, TIME, '\n'};    //Request time
+  
+  startTime = millis();
+  Serial.write(TMEmsg, 4);
+  bool ttest = false;
+  unsigned long regTime;
+  while(ttest == false)
+    {
+      while(!Serial){}                              //Wait for serial
+      if(Serial.read() == TRAIN_ID)
+      {
+        Serial.read();
+        if(Serial.read() == TIME_RESP)                     //Recieve TIME_RESP
+        {
+          endTime = millis();
+          regTime = Serial.read()<<24|Serial.read()<<16|Serial.read()<<8|Serial.read();
+          ttest = true;
+        }
+      }
+      
+    }
+  
+  timeOfDay = regTime * 1000; //calculate ofset value for millis()
+  millisOffset = startTime + (endTime - startTime)/2;
+  
 }
 
 
@@ -49,12 +92,7 @@ void measurementRelay::forwardIMU()
 
 
 
-//*********************************************************************************
-//
-//  Listen for RFID measurements over serial and IMU readings either from 
-//    interrupts or by request (selectable)
-//
-//*********************************************************************************
+
 
 void measurementRelay::listenForMeasurements()
 {
@@ -110,7 +148,7 @@ void measurementRelay::listenForMeasurements()
             Rmsg[4] = 0x4;
             Rmsg[5] = 0x5;
             Rmsg[6] = 0x6;
-            //test block till time implementation*********************************************************
+            //test block till timne implementation*********************************************************
 
             for (i=0; i<5; i++) {
               Rmsg[7+i] = code[i];
@@ -128,7 +166,9 @@ void measurementRelay::listenForMeasurements()
 
 
   //************************************************************************************************
+  // 
   // Use section below to operate linearly and not use interrupts
+  //
   //************************************************************************************************
 
   //Serial.println(BASE_ID + TRAIN_ID + IMU + millis() + AcX + AcY + AcZ + GyX + GyY + GyZ)
@@ -136,43 +176,34 @@ void measurementRelay::listenForMeasurements()
   int16_t Tmp;
   unsigned long tme;
   
-   
   byte msg [20];
-  
-  //debug loop
-  for(int i = 0; i < 20; i++)
-  {
-    msg[i] = i;
-  }
-  //*********
-
   msg[0] = BASE_ID;
   msg[1] = TRAIN_ID;
   msg[2] = IMU;
 
-  /*tme = timeOfDay + (millis() - millisOffset);
-  msg[3] = 0x03; //(int)((tme >> 24) & 0xFF) ;
-  msg[4] = 0x04; //(int)((tme >> 16) & 0xFF) ;
-  msg[5] = 0x05; //(int)((tme >> 8) & 0XFF);
-  msg[6] = 0x06; //(int)((tme & 0XFF));
+  tme = timeOfDay + (millis() - millisOffset);
+  msg[4] = 0x04; //(int)((tme >> 24) & 0xFF) ;
+  msg[5] = 0x05; //(int)((tme >> 16) & 0xFF) ;
+  msg[6] = 0x06; //(int)((tme >> 8) & 0XFF);
+  msg[7] = 0x07; //(int)((tme & 0XFF));
   
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
   Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
   
-  msg[7] = Wire.read(); msg[8] = Wire.read();     //AcX
-  msg[9] = Wire.read(); msg[10] = Wire.read();   //AcY
-  msg[11] = Wire.read(); msg[12] = Wire.read();   //AcZ
+  msg[8] = Wire.read(); msg[9] = Wire.read();     //AcX
+  msg[10] = Wire.read(); msg[11] = Wire.read();   //AcY
+  msg[12] = Wire.read(); msg[13] = Wire.read();   //AcZ
   
   Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
   
-  msg[13] = Wire.read(); msg[14] = Wire.read();     //GyX
-  msg[15] = Wire.read(); msg[16] = Wire.read();   //GyY
-  msg[17] = Wire.read(); msg[18] = Wire.read();   //GyZ
-  msg[19] = '\n';
+  msg[14] = Wire.read(); msg[15] = Wire.read();     //GyX
+  msg[16] = Wire.read(); msg[17] = Wire.read();   //GyY
+  msg[18] = Wire.read(); msg[19] = Wire.read();   //GyZ
+  msg[20] = '\n';
 
-  Serial.write(msg, 20);*/
+  Serial.write(msg, 20);
 }
 
 
@@ -183,130 +214,6 @@ void measurementRelay::linkIMU(imuDriver * imu)
 {
   
 }
-
-
-//*******************************************************************************
-//
-//  Send the ID message and completes the link procedure with the base computer
-//
-//*******************************************************************************
-
-void measurementRelay::baseLink()
-{
-  unsigned long timeout;
-  bool linkstatus = false;
-  int toDur = 1500;                                       //timeout duration in milliseconds
-  byte val = 0;
-
-  byte IDmsg [4] = {BASE_ID, TRAIN_ID, ID, '\n'};         //Build ID message
-    
-  while(!linkstatus)                                      //wait for ID_RESP message
-  {
-    Serial.write(IDmsg, 4);                                 //Send ID message
-    timeout = millis();
-    while((millis() - timeout) < toDur)                      //set timeout to 1.5 seconds
-    {
-      if(val = Serial.available() > 0)
-      {
-        byte head[2];
-
-        if(val = Serial.read() == TRAIN_ID)
-        {
-          Serial.readBytes(head, 2);
-          if(head[1] == ID_RESP)
-            {
-              linkstatus = true;
-            }
-        }
-      }   
-    } 
-  }
-}
-
-
-//********************************************************************************
-//
-//  Measures the RTT to the base computer
-//
-//********************************************************************************
-
-unsigned long measurementRelay::measureRTT()
-{
-  unsigned long startTime, endTime;
-  unsigned long latency = 0;
-  int measures = 0;
-  byte RTTmsg[4] = {BASE_ID, TRAIN_ID, RTT_REQ, '\n'};    //Build RTT_REQ
-  bool rtttest = false;
-  while(!rtttest)
-  {
-    startTime = millis();
-    Serial.write(RTTmsg, 4);
-    while(millis() - startTime < 1500)
-    {
-      if(Serial.available())
-      {
-        byte head[2];
-        if(Serial.read() == TRAIN_ID)
-        {
-          Serial.readBytes(head, 2);
-          if(head[1] == RTT_RESP)
-          {
-            latency = millis() - startTime;
-              rtttest = true;                    
-          }
-        }
-      } 
-    }
-    
-  }
-
-  return latency;
-}
-
-
-//***********************************************************************************
-//
-//  Link to the base computer's time of day. 
-//    Inputs: RTT of comunication link in milliseconds
-//
-//***********************************************************************************
-
-void measurementRelay::timeLink(unsigned long RTT)
-{
-   byte TMEmsg [4] = {BASE_ID, TRAIN_ID, TIME, '\n'};    //Request time
-  
-  bool ttest = false;
-  unsigned long regTime, endTime, startTime;
-  while(ttest == false)
-    {
-      Serial.write(TMEmsg, 4);
-      startTime = millis();
-      while(millis() - startTime < 1500)                              //Wait for serial
-      {
-        if(Serial.available())
-        {
-          byte head[2];
-          if(Serial.read() == TRAIN_ID)
-          {
-            Serial.readBytes(head, 2);
-            if(head[1] == TIME_RESP)                     //Recieve TIME_RESP
-            {
-              endTime = millis();
-              byte timeBuff[4];
-              Serial.readBytes(timeBuff, 4);
-              //regTime = timeBuff[0]<<24|timeBuff[1]<<16|timeBuff[2]<<8|timeBuff[3];
-              ttest = true;
-            }
-          }
-        }
-      }
-      
-    }
-  
-  timeOfDay = regTime * 1000; //calculate ofset value for millis()
-  millisOffset = startTime + (endTime - startTime)/2;
-}
-
 
 
 #endif
