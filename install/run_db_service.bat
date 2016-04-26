05e8c7fc4eec298f5f1f239d45c3cfbd2fcc6649
Cathlorn <sanderscorey@yahoo.com>
2016-04-25 22:29:53 -0500

    Updated the install JAR for the Navigation Service and fixed bug where nav service bat didn't work.

--- install/run_db_service.bat
+++ install/run_db_service.bat
@@ -1,1 +1,2 @@
 java -jar C:\TrainTrax\CPE656TL-master\install\TrainNavigationDatabase.RestService.jar
+pause
--- install/run_nav_service.bat
+++ install/run_nav_service.bat
@@ -1,1 +1,2 @@
-java -jar C:\TrainTrax\CPE656TL-master\install\TrainNavigationService.RestService.jar
+java "-Djava.library.path=C:\Program Files (x86)\JMRI\lib\x64" -Dfile.encoding=Cp1252  -jar "C:\TrainTrax\CPE656TL-master\install\TrainNavigationService.RestService.jar"
+pause
