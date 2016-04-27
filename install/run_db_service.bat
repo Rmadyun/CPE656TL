04bf8e7866251d4d1d9f78e51ba9cd61b406e763
death <cathlorn@gmail.com>
2016-04-27 18:15:43 -0500

    Refactored the installation / run scripts so that it is clearer how to configure Train Trax services to run.

--- install/run_db_service.bat
+++ install/run_db_service.bat
@@ -1,2 +1,3 @@
-java -jar C:\TrainTrax\CPE656TL-master\install\TrainNavigationDatabase.RestService.jar
+set "SERVICE_PORT=8182"
+java -jar "C:\TrainTrax\CPE656TL-master\install\TrainNavigationDatabase.RestService.jar" --service-port=%SERVICE_PORT% 
 pause
