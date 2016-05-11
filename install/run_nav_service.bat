ebd05ef285bb0ee2b788c7c1704b06118f8f120a
Cathlorn <sanderscorey@yahoo.com>
2016-05-11 05:58:48 -0500

    Updated the install jar file so that the --rfid-tag-only argument works. Enabled the --rfid-tag-only field by default.

--- install/run_nav_service.bat
+++ install/run_nav_service.bat
@@ -1,5 +1,5 @@
 set "PR3_PORT=COM4"
 set "MDU_PORT=COM5"
 set "SERVICE_PORT=8183"
-java "-Djava.library.path=C:\Program Files (x86)\JMRI\lib\x64" -Dfile.encoding=Cp1252  -jar "C:\TrainTrax\CPE656TL-master\install\TrainNavigationService.RestService.jar" --service-port=%SERVICE_PORT% --pr3-port=%PR3_PORT% --mdu-port=%MDU_PORT%
+java "-Djava.library.path=C:\Program Files (x86)\JMRI\lib\x64" -Dfile.encoding=Cp1252  -jar "C:\TrainTrax\CPE656TL-master\install\TrainNavigationService.RestService.jar" --service-port=%SERVICE_PORT% --pr3-port=%PR3_PORT% --mdu-port=%MDU_PORT% --rfid-tag-only
 pause
